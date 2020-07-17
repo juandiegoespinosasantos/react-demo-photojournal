e47b9929efeb33fa39e749440ec1968329299baf
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2020-07-16 21:04:05 -0500

    Updates

---
+++ precache-manifest.1568f6044c05b2bee923a4c566ce8514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1e037113cb5dda80111871136dfcac",
+    "url": "/react-demo-photojournal/index.html"
+  },
+  {
+    "revision": "78a4cd3434b8bd9e0b5d",
+    "url": "/react-demo-photojournal/static/css/main.74bec515.chunk.css"
+  },
+  {
+    "revision": "2c8cb2eb9abb5d16c975",
+    "url": "/react-demo-photojournal/static/js/2.9fc9cb40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-photojournal/static/js/2.9fc9cb40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a4cd3434b8bd9e0b5d",
+    "url": "/react-demo-photojournal/static/js/main.deed610b.chunk.js"
+  },
+  {
+    "revision": "043808e80b83719de88b",
+    "url": "/react-demo-photojournal/static/js/runtime-main.bb4ba42a.js"
+  }
+]);
